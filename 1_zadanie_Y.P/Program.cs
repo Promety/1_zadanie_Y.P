4cb42693e6b8672367fd9e09b5b7dc96835ed408
user <user@WIN-349MCH6R7UR>
2022-12-01 16:28:29 +0300

    Добавьте файлы проекта.

---
+++ 1_zadanie_Y.P/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1_zadanie_Y.P
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вычисление гипотенузы по двум катетам!
+            Console.WriteLine("Введите первый катет ");
+            double a;
+            while (!double.TryParse(Console.ReadLine(), out a) || a <= 0)
+                Console.WriteLine("Катет не может быть отрицательным!");
+            Console.WriteLine("Введите второй катет ");
+            double b;
+            while (!double.TryParse(Console.ReadLine(), out b) || b <= 0) 
+                Console.WriteLine("Катет не может быть отрицательным!");
+            double c;
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine("Гипотенуза равна {0}", c);
+        }
+    }
+}
